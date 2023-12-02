9d4ed8b1fa4d284334b73f3b9c828f85375510cc
LEE Hsin-ning <hleeau@ugcpu6.cse.ust.hk>
2023-12-03 00:55:06 +0800

    update rle_decoding and ClassId type

--- milestone_v0.py
+++ milestone_v0.py
@@ -16,63 +16,54 @@ import mmdet
 import mmcv
 import mmengine
 import subprocess
-import shutil
-from mmdet.apis import init_detector, inference_detector
-from utils import kaggle2coco
 
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+# Step 0: Get all filenames in the directory
+dir_path = "/data/hleeau/imaterialist_checkpoint/mmdetection/imaterialist/train"
+filenames = os.listdir(dir_path)
 
 # Step 1: Prepare train_df
 # ----------------------------------------------------------------------------------#
+# Read the CSV file
 file_dir = "mmdetection/imaterialist/train.csv"
-
-# Option 1: Retrieve the filenames from the CSV file
-# train_df_1 = pd.read_csv(file_dir, nrows=31)
-# print(train_df_1.head(31))
-# train_df_2 = pd.read_csv(file_dir, skiprows=range(1,301), nrows=60)
-# train_df = pd.concat([train_df_1, train_df_2], ignore_index=True)
-
-# Keep only the rows where 'ClassId' does not contain "_"
-# train_df_1 = train_df_1[~train_df_1['ClassId'].str.contains("_")]
-
-# image_ids = train_df_1["ImageId"].tolist()
-# image_ids = set(image_ids)
-# source_dir = "/data/hleeau/imaterialist_checkpoint/mmdetection/imaterialist/train"
-# target_dir = "/data/hleeau/imaterialist_checkpoint/mmdetection/imaterialist/testing"
-
-# for image_id in image_ids:
-#     source_file = os.path.join(source_dir, image_id)
-#     target_file = os.path.join(target_dir, image_id)
-#     shutil.copyfile(source_file, target_file)
-
-# Option 2: Retrieve the filenames from the directory
-dir_path = "/data/hleeau/imaterialist_checkpoint/mmdetection/imaterialist/train"
-filenames = os.listdir(dir_path)
 train_df = pd.read_csv(file_dir)
+# print(filenames)
 train_df = train_df[train_df['ImageId'].isin(filenames)]
+print(len(train_df))
 
 # Remove all the attributes after "_" in train_df['ClassId']
 if "_" in train_df['ClassId'].to_string():
   train_df['ClassId'] = train_df['ClassId'].str.split('_').str.get(0)
-
-train_df['ClassId'] = train_df['ClassId'].astype(int)
 
 # Pre-processing: create new column ImageId
 train_df = train_df.rename(columns={'ImageId': 'file_name'})
 train_df['ImageId'] = pd.factorize(train_df['file_name'])[0]
 
 def rle_decoding(encoding, height, width):
-    # return mask, area, bbox
+    """
+    Decodes RLE encoding into a binary mask and calculates the masked area and bbox.
+
+    Parameters:
+        encoding (str): RLE encoding as a space-separated string.
+        height (int): Height of the image.
+        width (int): Width of the image.
+
+    Returns:
+        mask (1d array): Decoded binary mask with shape (height, width).
+        area (float): Total number of pixels in the mask.
+        bbox (list): Bounding box coordinates [x, y, width, height].
+
+    """
+    # Remove space
     encoding = encoding.split()
 
     encoding = list(map(int, encoding))
 
-    generated_1d_mask = np.zeros(width*height)
+    generated_1d_mask = np.zeros(width*height).astype(int)
 
     for j in range(len(encoding) // 2):
       generated_1d_mask[encoding[j * 2]:encoding[j * 2]+encoding[j * 2+1]] = 1
 
-    binary_mask = generated_1d_mask.reshape((width, height)).T.astype(int)
+    mask = generated_1d_mask.reshape((width, height)).T.astype(int)
     area = float(np.sum(mask))
 
     # Find the coordinates of the foreground pixels
@@ -81,10 +72,10 @@ def rle_decoding(encoding, height, width):
     # Calculate the bounding box
     x_min, y_min = np.min(coords, axis=0)
     x_max, y_max = np.max(coords, axis=0)
-    # bbox = [x_min, y_min, x_max - x_min + 1, y_max - y_min + 1]
-    bbox = [y_min, x_min, y_max - y_min + 1, x_max - x_min + 1]
+    bbox = [x_min, y_min, x_max - x_min + 1, y_max - y_min + 1]
+    mask = mask.tolist()
 
-    return binary_mask, area, bbox
+    return mask, area, bbox
 
 # Create empty lists for the new columns
 segmentation_list = []
@@ -95,15 +86,14 @@ for i in tqdm(range(len(train_df))):
     encoding = train_df.iloc[i]['EncodedPixels']
     height = train_df.iloc[i]['Height']
     width = train_df.iloc[i]['Width']
-    poly, mask, area, bbox = rle_decoding(encoding, height, width)
-
-    rle = kaggle2coco(list(map(int, encoding.split())), height, width)
+    mask, area, bbox = rle_decoding(encoding, height, width)
 
     # Append the values to the respective lists
-    segmentation_list.append(rle)
+    segmentation_list.append(mask)
     area_list.append(area)
     bbox_list.append(bbox)
-
+    # if (i%100 == 0):
+    #     print("current:", i)
 
 # Assign the lists to the DataFrame columns
 train_df['segmentation'] = segmentation_list
@@ -150,7 +140,6 @@ unique_ids = train_df_unique.apply(lambda row: {'id': row['ImageId'],
                                                 'width': row['Width'],
                                                 'height': row['Height'],
                                                 'file_name': row['file_name']}, axis=1).tolist()
-
 print("Unique IDs: ", len(unique_ids))
 coco_base["images"] = unique_ids
 
@@ -174,8 +163,8 @@ coco_base["annotations"] = annotation_list
 # Convert the list of dictionaries to a JSON string
 # annotation_json = pd.DataFrame(annotation_list).to_json(orient='records')
 
-# Save the JSON string to a files
-# with open('mmdetection/imaterialist/train.json', 'w') as f:
+# # Save the JSON string to a files
+# with open('mmdetection/imaterialist/annotation.json', 'w') as f:
 #     f.write(annotation_json)
 
 class NpEncoder(json.JSONEncoder):
@@ -188,8 +177,8 @@ class NpEncoder(json.JSONEncoder):
             return obj.tolist()
         return super(NpEncoder, self).default(obj)
 
-out_file = open('mmdetection/imaterialist/train_coco.json','w')
-json.dump(coco_base, out_file, indent=4, cls=NpEncoder)
+out_file = open('mmdetection/imaterialist/coco_base100.json','w')
+json.dump(coco_base, out_file, cls=NpEncoder)
 out_file.close()
 # ----------------------------------------------------------------------------------#
 
@@ -223,12 +212,12 @@ cfg.metainfo = {
 # Modify dataset type and path
 cfg.data_root = 'mmdetection/imaterialist'
 
-cfg.train_dataloader.dataset.ann_file = 'train_coco.json'
+cfg.train_dataloader.dataset.ann_file = 'coco_base100.json'
 cfg.train_dataloader.dataset.data_root = cfg.data_root
 cfg.train_dataloader.dataset.data_prefix.img = 'train/'
 cfg.train_dataloader.dataset.metainfo = cfg.metainfo
 
-cfg.val_dataloader.dataset.ann_file = 'train_coco.json'
+cfg.val_dataloader.dataset.ann_file = 'coco_base100.json'
 cfg.val_dataloader.dataset.data_root = cfg.data_root
 cfg.val_dataloader.dataset.data_prefix.img = 'train/'
 cfg.val_dataloader.dataset.metainfo = cfg.metainfo
@@ -236,8 +225,8 @@ cfg.val_dataloader.dataset.metainfo = cfg.metainfo
 cfg.test_dataloader = cfg.val_dataloader
 
 # Modify metric config
-cfg.val_evaluator.ann_file = cfg.data_root+'/'+'train_coco.json'
-cfg.test_evaluator = cfg.val_evaluator
+cfg.val_evaluator.ann_file = cfg.data_root+'/'+'coco_base100.json'
+# cfg.test_evaluator = cfg.val_evaluators
 
 # Modify num classes of the model in box head and mask head
 cfg.model.roi_head.bbox_head.num_classes = 46
@@ -247,7 +236,7 @@ cfg.model.roi_head.mask_head.num_classes = 46
 cfg.load_from = 'mmdetection/checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
 
 # Set up working dir to save files and logs.
-cfg.work_dir = './train_result'
+cfg.work_dir = './milestone'
 
 # We can set the evaluation interval to reduce the evaluation times
 cfg.train_cfg.val_interval = 3
@@ -256,7 +245,7 @@ cfg.default_hooks.checkpoint.interval = 3
 
 # The original learning rate (LR) is set for 8-GPU training.
 # We divide it by 8 since we only use one GPU.
-cfg.optim_wrapper.optimizer.lr = 0.001
+cfg.optim_wrapper.optimizer.lr = 0.02
 cfg.default_hooks.logger.interval = 10
 
 
@@ -276,10 +265,3 @@ with open(config, 'w') as f:
 # Step 5: Called tool/train.py file with {config}
 # ----------------------------------------------------------------------------------#
 subprocess.call(["python", "mmdetection/tools/train.py", config])
-
-# Step 6: Test the model
-# img = mmcv.imread('mmdetection/imaterialist/train_15/0000fe7c9191fba733c8a69cfaf962b7.jpg',channel_order='rgb')
-# checkpoint_file = 'milestone/epoch_12.pth'
-# model = init_detector(cfg, checkpoint_file, device='cuda:0')
-# new_result = inference_detector(model, img)
-# print(new_result)
